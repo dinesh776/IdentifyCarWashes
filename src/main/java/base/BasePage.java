7599d4c857d5fae2b3ceeccb78ca95a41de69ee6
Dinesh <2421162@cognizant.com>
2025-08-20 15:23:01 +0530

    added service page

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,14 +1,17 @@
 package base;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
-    WebDriver driver;
+    public WebDriver driver;
+    public JavascriptExecutor jsExecutor;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
+        jsExecutor=(JavascriptExecutor) driver;
     }
 
 }
--- src/test/java/testcases/HomeTest.java
+++ src/test/java/testcases/HomeTest.java
@@ -3,15 +3,27 @@ package testcases;
 import base.BaseTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.ServicePage;
 
 public class HomeTest extends BaseTest {
 
-    @Test
+    @Test(priority = 1)
     public void testHome(){
         HomePage homePage=new HomePage(driver);
         homePage.handlePopup();
         homePage.setSearchField("Car Wash Services");
         homePage.clickOnSearch();
+    }
+
+    @Test(priority = 2)
+    public void testServicePage(){
+        ServicePage servicePage=new ServicePage(driver);
+        servicePage.clickFilters();
+        servicePage.setQuickFilter("top rated");
+        servicePage.sortBy("rating");
+        servicePage.setServiceType("authorised");
+        servicePage.setRating("4.5+");
+        servicePage.applyFilters();
     }
 
 }
