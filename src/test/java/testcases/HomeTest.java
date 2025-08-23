d14d47b371b2aaf24e77c4474b9649dfb3293a93
Dinesh <2421162@cognizant.com>
2025-08-23 17:10:34 +0530

    fixed some issues

--- src/test/java/testcases/HomeTest.java
+++ src/test/java/testcases/HomeTest.java
@@ -16,58 +16,68 @@ import java.util.List;
 
 public class HomeTest extends BaseTest {
 
-  @Test(priority=0)
-    public void homeTest(){
-      HomePage homePage=new HomePage(driver);
-       
-        homePage.handleLocation("Old Gajuwaka,Visakhapatnam");
-        homePage.setSearchField("Car Wash Services");
-        homePage.clickOnSearchWithWait();
-    }
+//  @Test(priority=0)
+//    public void homeTest(){
+//      HomePage homePage=new HomePage(driver);
+//       homePage.handlePopups();
+//        homePage.handleLocation("Old Gajuwaka,Visakhapatnam");
+//        homePage.setSearchField("Car Wash Services");
+//        homePage.clickOnSearchWithWait();
+//
+//      }
+//    }
+
+  public static void main(String[] args) {
+    List<HashMap<String ,String>> res=ExcelUtility.getTestData();
+    System.out.println("| S.No  | Location  | Service Name  | Quick Filters | Sort By | Ratings |");
+    for(HashMap<String,String> m : res){
+      System.out.println("| "+m.get("S.No")+" | "+m.get("Location")+" | "+m.get("Service Name")+" | "+m.get("Quick Filters")+" | "+m.get("Sort By")+" | "+m.get("Ratings")+" |");
+      System.out.println();
+  }}
 
 
   
-    @Test(priority = 1)
-    public void testServicePage(){
-        ServicePage servicePage=new ServicePage(driver);
-        servicePage.clickFilters();
-        servicePage.setQuickFilter("top rated");
-        servicePage.sortBy("rating");
-        servicePage.setRating("4.5+");
-        servicePage.applyFilters();
-    }
-
-    @Test(priority = 2)
-    public void getResults(){
-        ResultsPage resultsPage=new ResultsPage(driver);
-//        resultsPage.setName("dinesh");
-//        resultsPage.clickOnSendQuery();
-//        System.out.println("Step 1 :"+resultsPage.getMobileErrorMsg());
+//    @Test(priority = 1)
+//    public void testServicePage(){
+//        ServicePage servicePage=new ServicePage(driver);
+//        servicePage.clickFilters();
+//        servicePage.setQuickFilter("top rated");
+//        servicePage.sortBy("rating");
+//        servicePage.setRating("4.5+");
+//        servicePage.applyFilters();
+//    }
 //
-//        resultsPage.setName("");
-//        driver.navigate().refresh();
-//        resultsPage.setMobileNumber("9876543210");
-//        resultsPage.clickOnSendQuery();
-//        System.out.println("Step 2 : "+resultsPage.getNameErrorMsg());
+//    @Test(priority = 2)
+//    public void getResults(){
+//        ResultsPage resultsPage=new ResultsPage(driver);
+////        resultsPage.setName("dinesh");
+////        resultsPage.clickOnSendQuery();
+////        System.out.println("Step 1 :"+resultsPage.getMobileErrorMsg());
+////
+////        resultsPage.setName("");
+////        driver.navigate().refresh();
+////        resultsPage.setMobileNumber("9876543210");
+////        resultsPage.clickOnSendQuery();
+////        System.out.println("Step 2 : "+resultsPage.getNameErrorMsg());
+////
+////        resultsPage.setName("d");
+////        resultsPage.setMobileNumber("9876543212");
+////        resultsPage.clickOnSendQuery();
+////        System.out.println("Step 3 : "+resultsPage.getWelcomeMsg());
+////
+////        resultsPage.closeSendQueryDialog();
 //
-//        resultsPage.setName("d");
-//        resultsPage.setMobileNumber("9876543212");
-//        resultsPage.clickOnSendQuery();
-//        System.out.println("Step 3 : "+resultsPage.getWelcomeMsg());
+//        System.out.println(resultsPage.retrieveServices(5,20));
 //
-//        resultsPage.closeSendQueryDialog();
-
-        System.out.println(resultsPage.retrieveServices(5,20));
-
-    }
-  
-//    @Test(priority=2)
-    public void freeListing()  {
-        FreeListingPage flp = new FreeListingPage(driver);
-        flp.freeListingPage();
-        flp.mobileNumber("987654321");
-        flp.startNow();
-        System.out.println(flp.errorDisplay());
-    }
+//    }
+//
+////    @Test(priority=2)
+//    public void freeListing()  {
+//        FreeListingPage flp = new FreeListingPage(driver);
+//        flp.freeListingPage();
+//        flp.mobileNumber("987654321");
+//        flp.startNow();
+//        System.out.println(flp.errorDisplay());
+//    }
 
 }
